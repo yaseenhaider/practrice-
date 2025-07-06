97fb477c14bbdbb755e3fa689ad6c801ccd6353e
Yaseen Haider <yaseenhaiderabbas@gmail.com>
2025-07-06 20:16:21 +0500

    demo

--- DateDemo.java
+++ DateDemo.java
@@ -1,2 +1,29 @@
-class date
-public String month;
+class Date {
+
+    public String month;
+    public int day;
+    public int year; //a four digit number.
+
+    public void displayDate() {
+        System.out.println(month + " " + day + ", " + year);
+    }
+}
+
+public class DateDemo {
+
+    public static void main(String[] args) {
+        Date date1, date2;
+        date1 = new Date();
+        date1.month = "January";
+        date1.day = 21;
+        date1.year = 2003;
+        System.out.println("date1:");
+        date1.displayDate();
+        date2 = new Date();
+        date2.month = "July";
+        date2.day = 4;
+        date2.year = 1776;
+        System.out.println("date2:");
+        date2.displayDate();
+    }
+}
